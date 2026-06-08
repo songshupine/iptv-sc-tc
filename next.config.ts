943dec62a36839e653fecd9269ada9e10bef8b61
songshupine <songshupine@live.com>
2026-06-08 16:47:07 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: false,
-  async rewrites() {
-    return [
-      {
-        source: "/udpxy/:path*",
-        // 【核心】将路径中的冒号替换为双下划线，绕过网关拦截
-        destination: "/api/udpxy/:path*", 
-      },
-    ];
-  },
+  // 不要写 rewrites，让 Middleware 全权处理
 };
 
 export default nextConfig;
