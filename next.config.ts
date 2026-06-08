8cf4afb2648193030663b6652cb01a068c7b3049
songshupine <songshupine@live.com>
2026-06-08 16:59:49 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  trailingSlash: false,
-  // 不要写 rewrites，让 Middleware 全权处理
+  // 强制关闭尾部斜杠，防止 308 重定向
+  trailingSlash: false, 
+  async rewrites() {
+    return [
+      {
+        // 匹配 /udpxy/ 后面的所有内容（包括 IP:Port）
+        source: "/udpxy/:path*",
+        // 【关键】直接透传 :path*，Next.js 不会去解析里面的冒号和点
+        destination: "/api/udpxy/:path*", 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
