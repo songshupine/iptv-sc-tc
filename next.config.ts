287d9cdc63d565f4b473d903caf9d6d5b3abceeb
songshupine <songshupine@live.com>
2026-06-08 14:49:05 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,13 @@
-import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
+  trailingSlash: false,
   async rewrites() {
     return [
       {
-        // 匹配前端访问的路径，例如：/udpxy/192.168.2.30:8888
-        source: "/:rule_name(udpxy|udpxy_cmcc|udpxy_cun)/:domain/:path*",
-        
-        // 【核心修改】去掉 destination 中的 :rule_name
-        // 这样转发给云函数的路径就变成了：/api/udpxy/192.168.2.30:8888
-        destination: "/api/udpxy/:domain/:path*", 
+        // 匹配 /udpxy, /udpxy_cmcc 等路径
+        source: "/:rule_name(udpxy|udpxy_cmcc|udpxy_cun)",
+        // 转发给云函数根路径，不再在路径中传递 domain
+        destination: "/api/udpxy", 
       },
     ];
   },
 };
-
-export default nextConfig;
