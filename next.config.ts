c4265dfbc577e04b25ede720892c2a5847043695
songshupine <songshupine@live.com>
2026-06-09 13:36:52 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-async rewrites() {
-  return [
-    {
-      source: "/udpxy/:path*",
-      destination: "/api/udpxy/:path*",
-    },
-  ];
-},
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/udpxy/:path*",       // ✅ 必须包含斜杠
+        destination: "/api/udpxy/:path*",
+      },
+    ];
+  },
+};
+
+export default nextConfig;
