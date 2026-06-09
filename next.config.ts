2cfe8154e43105c0a6fb9891cac8568077213b74
songshupine <songshupine@live.com>
2026-06-09 13:04:18 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  //async redirects() {
-  //  return [
-  //    {
+  async redirects() {
+    return [
+      {
         // ✅ 使用通配符 :path* 捕获 udpxy 后的所有内容（包括参数）
-  //      source: "/udpxy/:path*",
+        source: "/udpxy/:path*",
         
         // ✅ 将捕获的内容原样传递到 api 路径下
-  //      destination: "/api/udpxy/:path*",
+        destination: "/api/udpxy/:path*",
         
         // ✅ 301 永久重定向，对 IPTV 播放器缓存更友好
-  //      permanent: true,
-  //    },
-  //  ];
-  //},
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
